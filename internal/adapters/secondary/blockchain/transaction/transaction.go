7d830b135d12287d98475c19ca7ea5ad60a7e492
AfroTechBoss <chidile.podcast@gmail.com>
2025-04-30 22:47:03 +0100

    build: add go-ethereum dependency for transaction handling
    This commit introduces the `github.com/ethereum/go-ethereum` package to support transaction-related functionalities in the blockchain adapter. The addition is necessary to utilize Ethereum-specific utilities and enhance transaction processing capabilities.

--- internal/adapters/secondary/blockchain/transaction/transaction.go
+++ internal/adapters/secondary/blockchain/transaction/transaction.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
